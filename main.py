89653e8a750716e22f6ff9019f156d1b8f939af1
tamar edri <tedri@g.jct.ac.il>
2024-06-05 18:44:59 +0300

    added functions to deal with abc files

---
+++ main.py
@@ -0,0 +1,9 @@
+#  pip install numpy pretty_midi seaborn matplotlib scikit-learn py-midi music21 re glob os
+import midi
+import glob
+import numpy as np
+import pretty_midi
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import LeaveOneOut
+
